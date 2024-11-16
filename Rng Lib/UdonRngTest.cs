36b2bd74e68f67e68ac4c8c31240712c3bc00386
WangQAQ123 <89381371+WangQAQ@users.noreply.github.com>
2024-11-16 21:43:36 +0800

    up
    up

---
+++ Rng Lib/UdonRngTest.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using WangQAQ.ED;
+
+public class UdonRngTest : UdonSharpBehaviour
+{
+    public string Rng;
+	public byte[] Rng1;
+    public long   Rng2;
+
+    public void Test()
+    {
+        Rng = UdonRng.GetRngSha256S();
+        Rng1 = UdonRng.GetRngSha256();
+        Rng2 = UdonRng.GetRng();
+	}
+}
---
+++ U#/HC256TEST.cs
@@ -0,0 +1,57 @@
+﻿//DEBUG
+/*
+ *  MIT License
+ *  Copyright (c) 2024 WangQAQ
+ */
+#if DEBUG
+using System;
+using System.Text;
+using UdonSharp;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace WangQAQ.ED
+{
+	public class HC256TEST : UdonSharpBehaviour
+	{
+		public HC256 _hc256;
+
+		public string InText = "";
+		public string Key = "";
+
+		public string IVb64 = "";
+		public string Outb64 = "";
+
+		public string Done = "";
+
+		public void Start()
+		{
+			if(_hc256 == null)
+				_hc256 = GameObject.Find("HC256").GetComponent<HC256>();
+		}
+
+		public void Encrypt()
+		{
+			byte[] iv = UdonRng.GetRngSha256();
+			IVb64 = Convert.ToBase64String(iv);
+
+			// Encrypt
+			var t = _hc256.Process(Encoding.UTF8.GetBytes(InText), Encoding.UTF8.GetBytes(UdonHashLib.SHA256_UTF8(Key)), iv);
+			Outb64 = Convert.ToBase64String(t);
+		}
+
+		public void Decrypt()
+		{
+			var iv = Convert.FromBase64String(IVb64);
+
+			//decrypt
+			var t = _hc256.Process(Convert.FromBase64String(Outb64), Encoding.UTF8.GetBytes(UdonHashLib.SHA256_UTF8(Key)), iv);
+			Done = Encoding.UTF8.GetString(t);
+		}
+
+	}
+}
+
+#endif
